4828dc994b8e6553b2a451d9c7966db97bcd40a1
Restyled.io <commits@restyled.io>
2021-02-25 06:01:56 +0000

    Restyled by black

--- setup.py
+++ setup.py
@@ -3,30 +3,30 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
 setup(
-    name='plasma',
-    description='Plasma MVP',
+    name="plasma",
+    description="Plasma MVP",
     long_description=readme,
-    author='David Knott',
-    author_email='',
+    author="David Knott",
+    author_email="",
     license=license,
-    packages=find_packages(exclude=('tests')),
+    packages=find_packages(exclude=("tests")),
     include_package_data=True,
     install_requires=[
-        'ethereum==2.3.0',
-        'web3==5.17.0',
-        'werkzeug==0.14.1',
-        'json-rpc==1.10.8',
-        'py-solc',
-        'pytest-cov',
-        'click==6.7',
-        'flake8==3.5.0',
-        'rlp==0.6.0'
+        "ethereum==2.3.0",
+        "web3==5.17.0",
+        "werkzeug==0.14.1",
+        "json-rpc==1.10.8",
+        "py-solc",
+        "pytest-cov",
+        "click==6.7",
+        "flake8==3.5.0",
+        "rlp==0.6.0",
     ],
     entry_points={
-        'console_scripts': ["omg=plasma.cli:cli"],
-    }
+        "console_scripts": ["omg=plasma.cli:cli"],
+    },
 )
